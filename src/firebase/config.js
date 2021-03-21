a17cb6123977bfc180e63c9c0898f0d2a9dec7d3
KillerBee05 <eherring0510@gmail.com>
2021-03-21 18:05:53 -0500

    config

---
+++ src/firebase/config.js
@@ -0,0 +1,13 @@
+import firebase from "firebase/app"
+
+const app = firebase.initializeApp({
+  apiKey: "AIzaSyCdd5SW1RgP0oct6-iWJMio0RQBSo3ko5Y",
+  authDomain: "portfolio-7ed56.firebaseapp.com",
+  projectId: "portfolio-7ed56",
+  storageBucket: "portfolio-7ed56.appspot.com",
+  messagingSenderId: "258032597616",
+  appId: "1:258032597616:web:70f12097878d92002cd2b5",
+  measurementId: "G-BNLE35FDSX"
+})
+
+export default app
